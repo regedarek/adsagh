fefed018c0ae28a2d6f013c694120ae11fdd212d
Bartek <bslipek@gmail.com>
2011-12-31 15:56:51 +0100

    Categories management in tree view

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -7,7 +7,13 @@ module CategoriesHelper
 
 	def nested_categories_tree(categories)
 		categories.map do |category, sub_category|
-			("--"*3*category.depth)+category.name + "<br/>"+ nested_categories_tree(sub_category)
+			c  =("--"*3*category.depth)
+			c << category.name + " "
+			c << link_to('e', edit_category_path(category))
+			c << link_to('d', category, :confirm => 'Are you sure?', :method => :delete) 
+			c << link_to("a", new_category_path(:parent_id => category))
+			c << "<br/>"
+			c << nested_categories_tree(sub_category)
 		end.join.html_safe
 	end
 end
